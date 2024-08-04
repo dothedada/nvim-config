174aac6c7fdde922a54bf02b4a3f6d849f355a24
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-08-04 11:33:50 -0500

    Actualiza configuración

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,12 +2,14 @@ return {
 	'ThePrimeagen/harpoon',
 
 	branch = 'harpoon2',
-	lazy = false,
-	requires = { 'nvim-lua/plenary.nvim' },
-	conf = function()
-		require('harpoon').setup {
-			-- save_on_ui_close = true,
-			-- save_on_toggle = true
-		}
-	end,
+
+	opts = {
+		menu = {
+			width = vim.api.nvim_win_get_width(0) - 4,
+		},
+		settings = {
+			save_on_toggle = true,
+		},
+	},
+	-- requires = { 'nvim-lua/plenary.nvim' },
 }
