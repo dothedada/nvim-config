0be5861a1c1df0f1a09fa51a45146223da1fd2b8
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-08-07 23:16:58 -0500

    Corrige bug en lint que ponia en conflicto los perfiles de linteo

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -8,34 +8,22 @@ return {
 	config = function()
 		local lint = require('lint')
 
-		lint.linters_by_ft = {
-			bash = { 'bash-language-server' },
-			zsh = { 'bash-language-server' },
-			javascript = { 'biome' },
-			typescript = { 'biome' },
-			javascriptreact = { 'eslint' },
-			typescriptreact = { 'eslint' },
-			svelte = { 'eslint' },
-			vue = { 'eslint' },
-		}
+		lint.linters_by_ft['markdown'] = { 'markdownlint' }
+		lint.linters_by_ft['bash'] = { 'bash-language-server' }
+		lint.linters_by_ft['zsh'] = { 'bash-language-server' }
+		lint.linters_by_ft['javascript'] = { 'biome' }
+		lint.linters_by_ft['typescript'] = { 'biome' }
+		lint.linters_by_ft['javascriptreact'] = { 'eslint' }
+		lint.linters_by_ft['typescriptreact'] = { 'eslint' }
+		lint.linters_by_ft['svelte'] = { 'eslint' }
+		lint.linters_by_ft['vue'] = { 'eslint' }
 
-		local lint_augroup =
-			vim.api.nvim_create_augroup('lint', { clear = true })
-
-		vim.api.nvim_create_autocmd(
-			{ 'BufEnter', 'BufWritePost', 'InsertLeave' },
-			{
-				group = lint_augroup,
-				callback = function()
-					lint.try_lint()
-				end,
-			}
-		)
-
-		vim.keymap.set('n', '<leader>lt', function()
-			lint.try_lint()
-		end)
+		local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+		vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
 	end,
-
-	enabled = false
 }
