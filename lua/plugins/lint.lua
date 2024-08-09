0f9db06b10426cab85a22c0c4f89b8dd98c99ce4
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-08-09 18:03:39 -0500

    Configira lint de nvim y resuelve conflictos de cuando no hay servidor de lint

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -6,36 +6,45 @@ return {
 	},
 
 	config = function()
-		local lint = require('lint')
+		local lint = require 'lint'
+
+		local projectLint = {}
+
+		if vim.fn.isdirectory 'node_modules/@biomejs' == 1 then
+			table.insert(projectLint, 'biomejs')
+		elseif vim.fn.isdirectory 'node_modules/@eslint' == 1 then
+			table.insert(projectLint, 'eslint')
+		end
+
+		if #projectLint == 0 then
+			print 'Sin lint'
+			return
+		end
+
+		print('JS/TS lint con: ' .. projectLint[1])
 
 		lint.linters_by_ft = {
-			bash = { 'bash-language-server' },
-			zsh = { 'bash-language-server' },
-			javascript = { 'biome' },
-			typescript = { 'biome' },
+			javascript = projectLint,
+			typescript = projectLint,
 			javascriptreact = { 'eslint' },
 			typescriptreact = { 'eslint' },
 			svelte = { 'eslint' },
 			vue = { 'eslint' },
 		}
 
-		local lint_augroup =
-			vim.api.nvim_create_augroup('lint', { clear = true })
+		local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
 
-		vim.api.nvim_create_autocmd(
-			{ 'BufEnter', 'BufWritePost', 'InsertLeave' },
-			{
-				group = lint_augroup,
-				callback = function()
-					lint.try_lint()
-				end,
-			}
-		)
+		vim.api.nvim_create_autocmd({ 'bufenter', 'bufwritepost', 'insertleave' }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
 
-		vim.keymap.set('n', '<leader>lt', function()
+		vim.keymap.set('n', '<leader>l', function()
 			lint.try_lint()
 		end)
 	end,
 
-	enabled = false
+	-- enabled = false
 }
