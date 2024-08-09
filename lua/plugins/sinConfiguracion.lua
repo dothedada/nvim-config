f4cabe34e5839c440ad5aac233999808dc9d8820
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-08-08 22:10:28 -0500

    Ordena plugins de poca configuracion, integra trouble a este archivo y elimina su archivo particular de configuración

--- lua/plugins/sinConfiguracion.lua
+++ lua/plugins/sinConfiguracion.lua
@@ -6,11 +6,11 @@ return {
 	{ 'kyazdani42/nvim-web-devicons', lazy = true },
 	{ 'numToStr/Comment.nvim', config = true, event = 'VeryLazy' },
 	{ 'lukas-reineke/indent-blankline.nvim', main = 'ibl', opts = {} },
-	{ 'numToStr/Comment.nvim', opts = {} },
 	{ 'windwp/nvim-autopairs', event = 'InsertEnter', opts = {} },
 	{ 'windwp/nvim-ts-autotag', opts = {} },
 	{ 'mbbill/undotree' },
 	{ 'folke/neodev.nvim', opts = {} },
+	{ 'folke/trouble.nvim', opts = {} },
 	{
 		'smjonas/inc-rename.nvim',
 
