9dd3f84070f91a6985691412137d1b8e60c113e0
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-08-09 15:45:51 -0500

    Limpia Mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,10 +1,19 @@
-local Plugin = { 'williamboman/mason.nvim' }
-
-Plugin.lazy = false
-
--- See :help mason-settings
-Plugin.opts = {
-	ui = { border = 'rounded' },
+return {
+	{
+		'williamboman/mason.nvim',
+		lazy = false,
+		config = function()
+			local mason = require('mason')
+			mason.setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+					border = 'rounded'
+				},
+			})
+		end
+	},
 }
-
-return Plugin
