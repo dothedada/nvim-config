a9489753f253e251fa39847d1e2ed46e6d371e4c
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-08-16 11:56:53 -0500

    Actualiza todo

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,42 +1,28 @@
 return {
 	'folke/todo-comments.nvim',
 	dependencies = { 'nvim-lua/plenary.nvim' },
+
 	opts = {
-		-- keywords recognized as todo comments
 		keywords = {
-			FIX = {
-				icon = ' ', -- icon used for the sign, and in search results
-				color = 'error', -- can be a hex color, or a named color (see below)
-				alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' }, -- a set of other keywords that all map to this FIX keywords
-				-- signs = false, -- configure signs for some keywords individually
-			},
+			FIX = { icon = ' ', color = 'error', alt = { 'BUG', 'ISSUE' } },
 			TODO = { icon = ' ', color = 'info' },
 			HACK = { icon = ' ', color = 'warning' },
 			WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
 			PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
 			NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
-			TEST = { icon = '⏲ ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
+			TEST = { icon = '󱦟 ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
 		},
-		-- list of named colors where we try to extract the guifg from the
-		-- list of highlight groups or use the hex color if hl not found as a fallback
 		colors = {
-			error = { '#DC2626' }, -- 'DiagnosticError', 'ErrorMsg', 
-			-- error = { 'DiagnosticError', 'ErrorMsg', '#DC2626' },
-			warning = { 'DiagnosticError', 'WarningMsg', '#FBBF24' },
-			-- warning = { '#7C3AED', 'DiagnosticWarn', 'WarningMsg', '#FBBF24' },
-			info = { '#2563EB' }, -- 'DiagnosticInfo',
-			hint = { '#10B981' }, -- 'DiagnosticHint', 
-			-- hint = { 'DiagnosticHint', '#10B981' },
-			default = { 'Identifier', '#7C3AED' },
-			test = { '#FF00FF' }, -- 'Identifier', 
-			-- test = { 'Identifier', '#FF00FF' },
+			error = { 'DiagnosticError', 'ErrorMsg', '#DC2626' },
+			warning = { 'DiagnosticWarn', 'WarningMsg', '#FBBF24' },
+			info = { 'DiagnosticInfo', '#2563EB' },
+			hint = { 'DiagnosticHint', '#10B981' },
+			default = { '#7C3AED' }, -- 'Identifier'
+			test = { 'Identifier', '#FF00FF' },
 		},
 
 		merge_keywords = true, -- when true, custom keywords will be merged with the defaults
-		-- highlighting of the line containing the todo comment
-		-- * before: highlights before the keyword (typically comment characters)
-		-- * keyword: highlights of the keyword
-		-- * after: highlights after the keyword (todo text)
+
 		highlight = {
 			multiline = true, -- enable multine todo comments
 			multiline_pattern = '^.', -- lua pattern to match the next multiline from the start of the matched keyword
