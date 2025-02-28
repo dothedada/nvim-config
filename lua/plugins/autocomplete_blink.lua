b6005ece1e0ccd5fda8433a46403fbb3c46fa9aa
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-02-27 22:23:54 -0500

    Setup the autocomplete

--- lua/plugins/autocomplete_blink.lua
+++ lua/plugins/autocomplete_blink.lua
@@ -20,8 +20,28 @@ return {
 				nerd_font_variant = 'mono',
 			},
 
-			signature = { enabled = true },
+			completion = {
+				menu = { border = 'rounded' },
+				documentation = { window = { border = 'rounded' } },
+			},
+
+			signature = {
+				enabled = true,
+				trigger = {
+					enabled = true,
+				},
+				window = {
+					min_width = 10,
+					max_width = 80,
+					max_height = 50,
+					border = 'rounded',
+					scrollbar = true,
+					treesitter_highlighting = true,
+					show_documentation = true,
+				},
+			}
 		},
 		opts_extend = { 'sources.default' }
 	},
 }
+
