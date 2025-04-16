9670248f4e274474c7fe2b0f77a842396ecd0e55
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-04-16 10:06:14 -0500

    refactor miniterm

--- lua/local_plugins/miniterm.lua
+++ lua/local_plugins/miniterm.lua
@@ -1,38 +1,70 @@
-local state = {
-	window = {
+local miniterm = {
+	state = {
 		buf = -1,
 		win = -1,
 	},
+
+	config = {
+		height = 10,
+		position = 'botright',
+		enter_insert = true,
+
+		win_opts = {
+			number = false,
+			relativenumber = false,
+			signcolumn = 'no',
+			cursorline = false,
+		},
+	},
 }
 
-local function create_bottom_term(opts)
+function miniterm.create_window(opts)
 	local buf = nil
+
 	if opts and opts.buf and vim.api.nvim_buf_is_valid(opts.buf) then
 		buf = opts.buf
 	else
 		buf = vim.api.nvim_create_buf(false, true)
 	end
 
-	vim.cmd 'botright 10split'
+	vim.cmd(
+		string.format(
+			'%s %dsplit',
+			miniterm.config.position,
+			miniterm.config.height
+		)
+	)
 
 	local win = vim.api.nvim_get_current_win()
 	vim.api.nvim_win_set_buf(win, buf)
-	vim.api.nvim_set_option_value('number', false, { win = win })
-	vim.api.nvim_set_option_value('relativenumber', false, { win = win })
 
-	return { buf = buf, win = win }
+	for option, value in pairs(miniterm.config.win_opts) do
+		vim.api.nvim_set_option_value(option, value, { win = win })
+	end
+
+	return { win = win, buf = buf }
 end
 
-local toggle_terminal = function()
-	if not vim.api.nvim_win_is_valid(state.window.win) then
-		state.window = create_bottom_term { buf = state.window.buf }
-		if vim.bo[state.window.buf].buftype ~= 'terminal' then
-			vim.cmd.terminal()
-		end
+function miniterm.toggle()
+	if
+		miniterm.state.win ~= -1
+		and vim.api.nvim_win_is_valid(miniterm.state.win)
+	then
+		vim.api.nvim_win_hide(miniterm.state.win)
+		return
+	end
+
+	miniterm.state = miniterm.create_window { buf = miniterm.state.buf }
+
+	if vim.bo[miniterm.state.buf].buftype ~= 'terminal' then
+		vim.cmd.terminal()
+	end
+
+	if miniterm.config.enter_insert then
 		vim.cmd 'startinsert'
-	else
-		vim.api.nvim_win_hide(state.window.win)
 	end
 end
 
-vim.api.nvim_create_user_command('Miniterm', toggle_terminal, {})
+vim.api.nvim_create_user_command('Miniterm', miniterm.toggle, {})
+
+return miniterm
